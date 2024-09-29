4cc9eedae2120adee97d00d9ef8d0df0df81e842
Oal304 <c.ola@alustudent.com>
2024-09-29 12:31:15 +0000

    updated

--- backend/django_project/settings.py
+++ backend/django_project/settings.py
@@ -1,9 +1,28 @@
 INSTALLED_APPS = [
-    # Other apps
-    'corsheaders',
+    # Default Django apps
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+
+    # Third-party apps
     'rest_framework',
-    'authentication',
+    'corsheaders',
+    'rest_framework_simplejwt.token_blacklist',  # For JWT handling
+    'django.contrib.sites',  # Required for django-allauth
+
+    # Authentication and Registration apps
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',  # Optional, if you're using social auth
+    'rest_auth',
+    
+    # Custom apps
+    'authentication',  # Your custom app for registration
 ]
+
 
 MIDDLEWARE = [
     # Other middlewares
