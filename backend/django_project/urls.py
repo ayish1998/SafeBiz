3d79545f5fbda41b184372c00a1c58f4855c921f
Oal304 <c.ola@alustudent.com>
2024-09-30 09:18:22 +0000

    updated

--- backend/django_project/urls.py
+++ backend/django_project/urls.py
@@ -6,10 +6,13 @@ from django.http import HttpResponse
 
 # Define a simple view function for the welcome message
 def welcome_view(request):
-    return HttpResponse("<h1>Welcome to the SentriBiz API</h1><p>This is the home page of the SentriBiz application.</p>")
+    return HttpResponse(
+        "<h1>Welcome to the SentriBiz API</h1>"
+        "<p>This is the home page of the SentriBiz application.</p>"       
+    )
 
 urlpatterns = [
-    path('', welcome_view, name='welcome'),  # Root URL that displays the welcome message
+    path('https://sentribiz-8az3.onrender.com', welcome_view, name='welcome'),  # Root URL that displays the welcome message
     path('api/assessment/', include('ai_integration.urls')),  # Include the AI integration URLs
     path('auth/', include('authentication.urls')),  # Include the authentication URLs
     path('admin/', admin.site.urls),  # Include the admin URLs
